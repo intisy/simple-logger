354a796687ea7b8f5bf7c08ad354871f19965e13
Pizza <wilderpizza99@gmail.com>
2024-05-03 15:47:55 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,15 @@ version = "1.2"
 repositories {
     mavenCentral()
 }
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            groupId = group.toString()
+            artifactId = "simple-logger" // Use a lowercase artifactId (convention)
+            version = version
+        }
+    }
+}
 
 tasks.jar {
     archiveFileName.set("SimpleLogger.jar") // Set the name of the JAR file here
