db8653e4072f9dc934e690b80f2b87e4bb3f4ed8
Intisy <wilderpizza99@gmail.com>
2024-09-25 14:48:32 +0200

    Update LoggerUtils.java

--- src/main/java/io/github/intisy/simple/logger/LoggerUtils.java
+++ src/main/java/io/github/intisy/simple/logger/LoggerUtils.java
@@ -1,5 +1,10 @@
 package io.github.intisy.simple.logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
 public class LoggerUtils {
     public static String exceptionToString(Exception exception) {
         return exceptionToString(exception, null);
@@ -15,5 +20,16 @@ public class LoggerUtils {
         }
         message = messageBuilder.toString();
         return message;
+    }
+    public static void response(SimpleLogger logger, HttpURLConnection response) throws IOException {
+        BufferedReader errorIn = new BufferedReader(new InputStreamReader(response.getErrorStream()));
+        String inputLine;
+        StringBuilder errorResponse = new StringBuilder();
+        while ((inputLine = errorIn.readLine()) != null) {
+            errorResponse.append(inputLine);
+        }
+        errorIn.close();
+        logger.note("Response Code: " + response.getResponseCode());
+        logger.note("Response Body: " + errorResponse);
     }
 }
